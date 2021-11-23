cce9a8372fd49e12da8fccc9fa25b012efd46219
rafi-e <75981512+rafi-e@users.noreply.github.com>
2021-11-23 16:00:19 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,84 @@
+# Program Management Kontak
+
+def display_kontak(daftar_kontak):
+    for kontak in daftar_kontak:
+        print("======================")
+        print(f"Nama : {kontak['nama']}")
+        print(f"Email : {kontak['email']}")
+        print(f"Telepon : {kontak['telepon']}")
+
+def new_kontak():
+    nama = input("Nama : ")
+    email = input("Email : ")
+    telepon = input("Telepon : ")
+    kontak = {
+        "nama" : nama,
+        "email" : email,
+        "telepon" : telepon
+    }
+    return kontak
+
+def hapus_kontak(daftar_kontak):
+    telepon = input("No telepon yang akan dihapus : ")
+
+    index = -1
+
+    for i in range(0, len(daftar_kontak)):
+        kontak = daftar_kontak[i]
+        if telepon == kontak["telepon"]:
+            index = i 
+            break
+
+    if index == -1:
+        print("Data kontak tidak ditemukan")
+    else:
+        del daftar_kontak[index]
+        print("Berhasil menghapus data kontak")
+
+def cari_kontak(daftar_kontak):
+    nama_yg_dicari = input("Nama yang dicari : ")
+
+    for kontak in daftar_kontak:
+        nama = kontak["nama"]
+        if nama.lower().find(nama_yg_dicari.lower()) != -1:
+            print("======================")
+            print(f"Nama : {kontak['nama']}")
+            print(f"Email : {kontak['email']}")
+            print(f"Telepon : {kontak['telepon']}")
+        else:
+            print("Kontak tidak ada")
+
+# List of dictionary
+daftar_kontak = []
+daftar_kontak.append({
+    "nama" : "Rafie Dwie Sapoetra",
+    "email" : "monkeyd.raffie@gmail.com",
+    "telepon" : "081213884921"
+})
+
+# Menu program
+while True:
+    print("# Menu")
+    print("1. Daftar Kontak")
+    print("2. Tambah Kontak")
+    print("3. Hapus Kontak")
+    print("4. Cari Kontak")
+    print("0. Keluar Program")
+
+    menu = input("Pilih menu : ")
+
+    if menu == "0":
+        break
+    elif menu == "1":
+        function.display_kontak(daftar_kontak)
+    elif menu == "2":
+        kontak = function.new_kontak()
+        daftar_kontak.append(kontak)
+    elif menu == "3":
+        function.hapus_kontak(daftar_kontak)
+    elif menu == "4":
+        function.cari_kontak(daftar_kontak)
+    else:
+        print("Menu tidak tersedia")
+
+print("Program selesai berjalan, sampai jumpa")
